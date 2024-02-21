f2a4ad5a3df0da134c62572720df211300e5691a
mappo0019 <71126432+mappo0019@users.noreply.github.com>
2024-02-21 17:48:05 +0100

    Update FactoriaCarretera.java

--- Ejercicio1-Java/FactoriaCarretera.java
+++ Ejercicio1-Java/FactoriaCarretera.java
@@ -8,6 +8,23 @@ package ds.prac1;
  *
  * @author Usuario
  */
-public class FactoriaCarretera {
+import ds.prac1.BicicletaCarretera;
+import ds.prac1.CarreraCarretera;
+public class FactoriaCarretera implements FactoriaCarrerayBicicleta{
     
-}
+    public FactoriaCarretera(){
+        
+    }
+    
+    @Override
+    public void crearCarrera(){
+        CarreraCarretera Carrera1 = new CarreraCarretera();
+        
+    }
+    
+    @Override
+    public void crearBicicleta(){
+        BicicletaCarretera Bici1 = new BicicletaCarretera();
+        
+    }
+    
