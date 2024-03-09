2b4be9790fba43ac72898ab2fbb89ab694874925
mappo0019 <71126432+mappo0019@users.noreply.github.com>
2024-03-09 16:19:18 +0100

    Add files via upload

---
+++ Práctica 1/FiltroCalcularVelocidad.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prac4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FiltroCalcularVelocidad implements Filtro{
+    
+    @Override
+    public double ejecutar(double revoluciones, EstadoMotor estadomotor){
+        return 0;
+    }
+    
+}
