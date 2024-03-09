fd502a6fe23042a1936fa9f54c8e435a5e78ddee
mappo0019 <71126432+mappo0019@users.noreply.github.com>
2024-03-09 16:21:30 +0100

    Esqueleto del ejercicio 4

---
+++ Práctica 1/Ejercicio4/Filtro.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package prac4;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface Filtro {
+    
+    public double ejecutar(double revoluciones, EstadoMotor estadomotor);
+    
+}
