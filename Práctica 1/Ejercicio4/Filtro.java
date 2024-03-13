430cc3a6d1889137ced14c603cf84abcc8ca616b
mappo0019 <71126432+mappo0019@users.noreply.github.com>
2024-03-13 17:04:36 +0100

    Update Filtro.java

--- Práctica 1/Ejercicio4/Filtro.java
+++ Práctica 1/Ejercicio4/Filtro.java
@@ -8,32 +8,8 @@ package prac4;
  *
  * @author Usuario
  */
-public class FiltroCalcularVelocidad implements Filtro{
+public interface Filtro {
     
-    @Override
-    public double ejecutar(double revoluciones, EstadoMotor estadomotor){
-        int incrementoVelocidad;
-        double devolver;
-        
-        if (estadomotor == EstadoMotor.ACELERANDO){
-            incrementoVelocidad = 100;
-            if(revoluciones < 4900)
-                devolver= revoluciones + incrementoVelocidad;
-            else
-                devolver= revoluciones;            
-        }
-        else if (estadomotor == EstadoMotor.FRENANDO){
-            incrementoVelocidad = -100;
-            if (revoluciones > 100)
-                devolver = revoluciones + incrementoVelocidad;           
-            else
-                devolver = 0;           
-        }        
-        else{
-            devolver = -1;   
-        }
-        return devolver;
-      
-    }
+    public double ejecutar(double revoluciones, EstadoMotor estadomotor);
     
 }
