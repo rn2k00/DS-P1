0913636263f88850d8ca4bd06bcb39c106db38c1
Rene Morales <92979005+rn2k00@users.noreply.github.com>
2024-03-19 10:18:10 +0100

    Update main.py

--- Práctica 1/Ejercicio5/main.py
+++ Práctica 1/Ejercicio5/main.py
@@ -1,2 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
+#Se importan los módulos necesarios para el scraping y el manejo de archivos JSON.
+
+
+import json 
+import requests  # Importa el módulo requests para realizar solicitudes HTTP
+from bs4 import BeautifulSoup  # Analizar la estructura HTML de las páginas web y extraer la información deseada
+
+# Clase principal para el web scraper
+class WebScraper:
+
+    #Constructor
+    def __init__(self, estrategia):
+        self.estrategia = estrategia
+
+    def extraer_datos(self, simbolo):
+        return self.estrategia.extraer_datos(simbolo)
+
+
+# Estrategia utilizando BeautifulSoup para el web scraping
+class EstrategiaBeautifulSoup:
