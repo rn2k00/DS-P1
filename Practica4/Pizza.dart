319c47c9dd63c20f474bdab93daf68d5864df15b
mappo0019 <71126432+mappo0019@users.noreply.github.com>
2024-05-22 16:52:00 +0200

    Create Pizza.dart

---
+++ Practica4/Pizza.dart
@@ -0,0 +1,42 @@
+class Pizza {
+  int? id;
+  List<String>? ing;
+  String? salsa;
+  String? tipoMasa;
+  String? tamanio;
+  String? nombre;
+  String? user;
+  bool? gluten;
+
+  Pizza({this.id, this.ing, this.salsa, this.tipoMasa, this.tamanio, this.nombre, this.user, this.gluten});
+
+  factory Pizza.fromJson(Map<String, dynamic> json) {
+    return Pizza(
+      id: json['id'] as int?,
+      ing: json['ing'] as List<String>,
+      salsa: json['salsa'] as String?,
+      tipoMasa: json['tipoMasa'] as String?,
+      tamanio: json['tamanio'] as String?,
+      nombre: json['nombre'] as String?,
+      user: json['user'] as String?,
+      gluten: json['gluten'] as bool?
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      if (id != null) 'id': id,
+      'salsa': salsa,
+      'tipoMasa': tipoMasa,
+      'Tamanio': tamanio,
+      'nombre': nombre,
+      'user': user,
+      'gluten' : gluten
+    };
+  }
+
+  @override
+  String toString() {
+    return "Ingredientes: $ing\nSalsa: $salsa\nTipo de masa: $tipoMasa\nTamaño: $tamanio\n${gluten ?? false ? "Con Gluten" : "Sin Gluten"}";
+  }
+}
